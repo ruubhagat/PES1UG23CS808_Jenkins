870b6a534d9843b445f498449dffdf6ba450ff1d
Rutuja Bhagat <rutujabhagatco2021@gmail.com>
2025-03-11 16:24:11 +0530

    Update hello.cpp with new output message

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Made change \n";
+  cout << "Made change1 \n";
   return 0;
 }
